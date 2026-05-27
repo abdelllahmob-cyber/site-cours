154d6dc0e86fc4b668e61f8a19e0abd8f92f9cb8
abdelllahmob-cyber <abdelllahmob@gmail.com>
2026-05-27 03:23:45 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 //  CONFIG — Supabase
 // ══════════════════════════════════════════════════════
 
-export const SUPABASE_URL  = "https://jpczouzgfzzsxnjqpvg.supabase.co";
-export const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwY3pvdXpnZmd6enN4bmpxcHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MjU4MzYsImV4cCI6MjA5NTQwMTgzNn0.ydRbz6IOSuaUrBdosNQ7vvVLt3bj46aDIo_MZyMPaV0";
+export const SUPABASE_URL  = "https://jpczouzgfgzzsxnjqpvg.supabase.co";
+export const SUPABASE_ANON = "sb_publishable_DQ3SyeeV_h9cpg334uC0oQ_d2UWEMMu";
 
 export const SITE_NAME = "Plateforme de Cours";
